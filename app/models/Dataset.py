7b7e939fc1c61ba0ad55dfb20c509de1e1ff8c76
Kirill Karpunin <kirill.karpunin20014@gmail.com>
2025-02-23 13:23:05 +0300

    [UPD] Minor updates

--- app/models/Dataset.py
+++ app/models/Dataset.py
@@ -4,8 +4,7 @@ from app.models.DatasetFormValues import DatasetFormValues
 class Dataset:
     def __init__(self, form_values: DatasetFormValues):
         self.dataset_name: str = form_values.dataset_name
-
-        self.dataset_description: str = form_values.dataset_description if form_values.dataset_description != '' else '(пусто)'
+        self.dataset_description: str = form_values.dataset_description
         self.dataset_type: str = form_values.dataset_type
 
     def to_dict(self):
